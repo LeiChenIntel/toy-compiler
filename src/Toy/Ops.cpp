facd08353e1c9f513cf4f151150a77c5e93d59f1
Lei, Chen <chenlei_imail@163.com>
2023-08-08 14:40:50 +0800

    Add operation format.

--- src/Toy/Ops.cpp
+++ src/Toy/Ops.cpp
@@ -1,5 +1,7 @@
 #include "Toy/Dialect.h"
 
+#include <mlir/IR/FunctionImplementation.h>
+
 #define GET_OP_CLASSES
 #include "Toy/Ops.cpp.inc"
 
@@ -15,6 +17,26 @@ void FuncOp::build(mlir::OpBuilder &builder, mlir::OperationState &state,
   // FunctionOpInterface provides a convenient `build` method that will populate
   // the state of our FuncOp, and create an entry block.
   buildWithEntryBlock(builder, state, name, type, attrs, type.getInputs());
+}
+
+mlir::ParseResult FuncOp::parse(mlir::OpAsmParser &parser,
+                                mlir::OperationState &result) {
+  auto buildFuncType =
+      [](mlir::Builder &builder, llvm::ArrayRef<mlir::Type> argTypes,
+         llvm::ArrayRef<mlir::Type> results,
+         mlir::function_interface_impl::VariadicFlag,
+         std::string &) { return builder.getFunctionType(argTypes, results); };
+
+  return mlir::function_interface_impl::parseFunctionOp(
+      parser, result, /*allowVariadic=*/false,
+      getFunctionTypeAttrName(result.name), buildFuncType,
+      getArgAttrsAttrName(result.name), getResAttrsAttrName(result.name));
+}
+
+void FuncOp::print(mlir::OpAsmPrinter &p) {
+  mlir::function_interface_impl::printFunctionOp(
+      p, *this, /*isVariadic=*/false, getFunctionTypeAttrName(),
+      getArgAttrsAttrName(), getResAttrsAttrName());
 }
 
 } // namespace toy
