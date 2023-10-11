77e70d85d1b85d8ec30f84ad76756c84bda1902c
Lei, Chen <chenlei_imail@163.com>
2023-10-11 18:29:32 +0800

    Convert Toy Constant to MidIR.

--- src/Conversion/Passes.cpp
+++ src/Conversion/Passes.cpp
@@ -124,6 +124,72 @@ public:
 using ToyAddPattern = ToyBinaryPattern<toy::AddOp, arith::AddFOp>;
 using ToyMulPattern = ToyBinaryPattern<toy::MulOp, arith::MulFOp>;
 
+class ToyConstantOpPattern : public OpConversionPattern<toy::ConstantOp> {
+  using OpConversionPattern<toy::ConstantOp>::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(toy::ConstantOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    DenseElementsAttr constantValue = op.getValue();
+    Location loc = op.getLoc();
+
+    // When lowering the constant operation, we allocate and assign the constant
+    // values to a corresponding memref allocation.
+    auto tensorType = op.getType().cast<TensorType>();
+    auto memRefType = convertTensorToMemRef(tensorType);
+    auto alloc = insertAllocAndDealloc(memRefType, loc, rewriter);
+
+    // We will be generating constant indices up-to the largest dimension.
+    // Create these constants up-front to avoid large amounts of redundant
+    // operations.
+    auto valueShape = memRefType.getShape();
+    SmallVector<Value, 8> constantIndices;
+
+    if (!valueShape.empty()) {
+      for (auto i : llvm::seq<int64_t>(
+               0, *std::max_element(valueShape.begin(), valueShape.end())))
+        constantIndices.push_back(
+            rewriter.create<arith::ConstantIndexOp>(loc, i));
+    } else {
+      // This is the case of a tensor of rank 0.
+      constantIndices.push_back(
+          rewriter.create<arith::ConstantIndexOp>(loc, 0));
+    }
+
+    // The constant operation represents a multi-dimensional constant, so we
+    // will need to generate a store for each of the elements. The following
+    // functor recursively walks the dimensions of the constant shape,
+    // generating a store when the recursion hits the base case.
+    SmallVector<Value, 2> indices;
+    auto valueIt = constantValue.value_begin<FloatAttr>();
+    std::function<void(uint64_t)> storeElements = [&](uint64_t dimension) {
+      // The last dimension is the base case of the recursion, at this point
+      // we store the element at the given index.
+      if (dimension == valueShape.size()) {
+        rewriter.create<AffineStoreOp>(
+            loc, rewriter.create<arith::ConstantOp>(loc, *valueIt++), alloc,
+            llvm::ArrayRef(indices));
+        return;
+      }
+
+      // Otherwise, iterate over the current dimension and add the indices to
+      // the list.
+      for (uint64_t i = 0, e = valueShape[dimension]; i != e; ++i) {
+        indices.push_back(constantIndices[i]);
+        storeElements(dimension + 1);
+        indices.pop_back();
+      }
+    };
+
+    // Start the element storing recursion from the first dimension.
+    storeElements(/*dimension=*/0);
+
+    // Replace this operation with the generated alloc.
+    rewriter.replaceOp(op, alloc);
+    return success();
+  }
+};
+
 class ToyFuncOpPattern : public OpConversionPattern<toy::FuncOp> {
   using OpConversionPattern<toy::FuncOp>::OpConversionPattern;
 
@@ -220,7 +286,8 @@ public:
     // the set of patterns that will lower the Toy operations.
     RewritePatternSet patterns(&getContext());
     patterns.add<ToyFuncOpPattern, ToyReturnOpPattern, ToyPrintOpPattern,
-                 ToyAddPattern, ToyMulPattern>(&getContext());
+                 ToyAddPattern, ToyMulPattern, ToyConstantOpPattern>(
+        &getContext());
 
     // With the target and rewrite patterns defined, we can now attempt the
     // conversion. The conversion will signal failure if any of our `illegal`
--- test_lit/Conversion/toy_to_mid.mlir
+++ test_lit/Conversion/toy_to_mid.mlir
@@ -54,3 +54,30 @@ toy.func @convert_mul_to_mid(%arg0: tensor<2x3xf64>, %arg1: tensor<2x3xf64>) {
     // CHECK: memref.dealloc %alloc : memref<2x3xf64>
     // CHECK: return
 }
+
+// CHECK-LABEL: @convert_constant_to_mid
+toy.func @convert_constant_to_mid() {
+    %0 = toy.constant {value = dense<[[2.000000e+00, 4.000000e+00, 6.000000e+00], [8.000000e+00, 1.000000e+01, 1.200000e+01]]> : tensor<2x3xf64>} : tensor<2x3xf64>
+    toy.print(%0) : tensor<2x3xf64>
+    toy.return
+
+    // CHECK: %alloc = memref.alloc() : memref<2x3xf64>
+    // CHECK: %c0 = arith.constant 0 : index
+    // CHECK: %c1 = arith.constant 1 : index
+    // CHECK: %c2 = arith.constant 2 : index
+    // CHECK: %cst = arith.constant 2.000000e+00 : f64
+    // CHECK: affine.store %cst, %alloc[%c0, %c0] : memref<2x3xf64>
+    // CHECK: %cst_0 = arith.constant 4.000000e+00 : f64
+    // CHECK: affine.store %cst_0, %alloc[%c0, %c1] : memref<2x3xf64>
+    // CHECK: %cst_1 = arith.constant 6.000000e+00 : f64
+    // CHECK: affine.store %cst_1, %alloc[%c0, %c2] : memref<2x3xf64>
+    // CHECK: %cst_2 = arith.constant 8.000000e+00 : f64
+    // CHECK: affine.store %cst_2, %alloc[%c1, %c0] : memref<2x3xf64>
+    // CHECK: %cst_3 = arith.constant 1.000000e+01 : f64
+    // CHECK: affine.store %cst_3, %alloc[%c1, %c1] : memref<2x3xf64>
+    // CHECK: %cst_4 = arith.constant 1.200000e+01 : f64
+    // CHECK: affine.store %cst_4, %alloc[%c1, %c2] : memref<2x3xf64>
+    // CHECK: toy.print(%alloc) : memref<2x3xf64>
+    // CHECK: memref.dealloc %alloc : memref<2x3xf64>
+    // CHECK: return
+}
