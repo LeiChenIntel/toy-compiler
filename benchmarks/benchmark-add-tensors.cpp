a438a934ad7b0067442d343c21a3b19ff8ea4f23
Lei, Chen <chenlei_imail@163.com>
2023-12-05 20:03:40 +0800

    Add simple loop and avx2 benchmarks.

---
+++ benchmarks/benchmark-add-tensors.cpp
@@ -0,0 +1,93 @@
+#include <chrono>
+#include <iostream>
+#include <random>
+#include <vector>
+
+#include <immintrin.h>
+
+double *createDoubleBuffer(const int n) {
+  double *ptr = (double *)malloc(n * sizeof(double));
+  std::default_random_engine gen;
+  std::uniform_real_distribution<double> dis(-10.0, 10.0);
+  for (int i = 0; i < n; i++) {
+    ptr[i] = dis(gen);
+  }
+  return ptr;
+}
+
+double *createDoubleAlignedBuffer(const int n) {
+  double *ptr = (double *)_mm_malloc(n * sizeof(double), 32);
+  std::default_random_engine gen;
+  std::uniform_real_distribution<double> dis(-10.0, 10.0);
+  for (int i = 0; i < n; i++) {
+    ptr[i] = dis(gen);
+  }
+  return ptr;
+}
+
+int main() {
+  const std::vector<int> elementNum{8192, 16384, 32768, 65536, 131072, 262144};
+  const int iter = 100;
+  std::vector<long long> loopResults;
+  std::vector<long long> avx2Results;
+
+  // Loop and not aligned
+  for (auto &n : elementNum) {
+    long long tcnt = 0;
+    for (int t = 0; t < iter; t++) {
+      double *src1 = createDoubleBuffer(n);
+      double *src2 = createDoubleBuffer(n);
+      double *dst = createDoubleBuffer(n);
+
+      const auto start = std::chrono::system_clock::now();
+      for (int i = 0; i < n; i++) {
+        dst[i] = src1[i] + src2[i];
+      }
+      const auto end = std::chrono::system_clock::now();
+      const auto last =
+          std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+      tcnt += last.count();
+
+      free(src1);
+      free(src2);
+      free(dst);
+    }
+
+    printf("[ADD LOOP NOT ALIGNED] Elements: %6d", n);
+    printf(" Time: %lldus\n", tcnt / iter);
+  }
+
+  // AVX2 and aligned
+  for (auto &n : elementNum) {
+    long long tcnt = 0;
+    for (int t = 0; t < iter; t++) {
+      double *src1 = createDoubleAlignedBuffer(n);
+      double *src2 = createDoubleAlignedBuffer(n);
+      double *dst = createDoubleAlignedBuffer(n);
+
+      const auto start = std::chrono::system_clock::now();
+      const int ndiv4 = n / 4;
+      for (int i = 0; i < ndiv4; i++) {
+        __m256d c1 = _mm256_load_pd(src1 + 4 * i);
+        __m256d c2 = _mm256_load_pd(src2 + 4 * i);
+        __m256d d = _mm256_add_pd(c1, c2);
+        _mm256_store_pd(dst + 4 * i, d);
+      }
+      for (int i = 4 * ndiv4; i < n; i++) {
+        dst[i] = src1[i] + src2[i];
+      }
+      const auto end = std::chrono::system_clock::now();
+      const auto last =
+          std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+      tcnt += last.count();
+
+      _mm_free(src1);
+      _mm_free(src2);
+      _mm_free(dst);
+    }
+    printf("[ADD AVX2 ALIGNED] Elements: %6d", n);
+    printf(" Time: %lldus\n", tcnt / iter);
+  }
+
+  return 0;
+}
