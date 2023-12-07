b17e37a0f947345717a775bfb885e83c62e95d53
Lei, Chen <chenlei_imail@163.com>
2023-12-07 14:58:16 +0800

    Add accuracy checker.

--- benchmarks/benchmark-add-tensors.cpp
+++ benchmarks/benchmark-add-tensors.cpp
@@ -1,5 +1,4 @@
-#include "ToyFrontend/Container.h"
-
+#include <cassert>
 #include <chrono>
 #include <iostream>
 #include <random>
@@ -32,6 +31,17 @@ double *createDoubleAlignedBuffer(const int n) {
   return ptr;
 }
 
+bool checkAccuracy(double *dst, double *src1, double *src2, int n) {
+  for (int i = 0; i < n; i++) {
+    double ref = src1[i] + src2[i];
+    if (ref != dst[i]) {
+      printf("Wrong Accuracy in index [%d]\n", i);
+      return false;
+    }
+  }
+  return true;
+}
+
 int main() {
   const std::vector<int> elementNum{8192, 16384, 32768, 65536, 131072, 262144};
   const int iter = 100;
@@ -55,6 +65,7 @@ int main() {
           std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
       tcnt += last.count();
 
+      assert(checkAccuracy(dst, src1, src2, n) == true);
       free(src1);
       free(src2);
       free(dst);
@@ -74,14 +85,22 @@ int main() {
 
       const auto start = std::chrono::high_resolution_clock::now();
       int i = 0;
-      __m256d c1;
-      __m256d c2;
-      __m256d d;
-      for (; i < n - 4; i += 4) {
-        c1 = _mm256_load_pd(src1 + i);
-        c2 = _mm256_load_pd(src2 + i);
-        d = _mm256_add_pd(c1, c2);
-        _mm256_store_pd(dst + i, d);
+      __m256d c1, c2, c3, c4;
+      double *p1 = src1 + i;
+      double *p2 = src2 + i;
+      double *q1 = dst + i;
+      for (; i < n - 8; i += 8) {
+        c1 = _mm256_load_pd(p1);
+        c2 = _mm256_load_pd(p2);
+        c3 = _mm256_load_pd(p1 + 4);
+        c4 = _mm256_load_pd(p2 + 4);
+        c1 = _mm256_add_pd(c1, c2);
+        c3 = _mm256_add_pd(c3, c4);
+        _mm256_store_pd(q1, c1);
+        _mm256_store_pd(q1 + 4, c3);
+        p1 += 8;
+        p2 += 8;
+        q1 += 8;
       }
       for (; i < n; i++) {
         dst[i] = src1[i] + src2[i];
@@ -91,6 +110,7 @@ int main() {
           std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
       tcnt += last.count();
 
+      assert(checkAccuracy(dst, src1, src2, n) == true);
       _mm_free(src1);
       _mm_free(src2);
       _mm_free(dst);
@@ -115,6 +135,7 @@ int main() {
           std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
       tcnt += last.count();
 
+      assert(checkAccuracy(dst, src1, src2, n) == true);
       _mm_free(src1);
       _mm_free(src2);
       _mm_free(dst);
