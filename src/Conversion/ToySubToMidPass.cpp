612cbc3f5922e121e9df3fa2816191c9ccc05f0c
Lei, Chen <chenlei_imail@163.com>
2024-03-18 20:33:35 +0800

    Backup.

--- src/Conversion/ToySubToMidPass.cpp
+++ src/Conversion/ToySubToMidPass.cpp
@@ -7,16 +7,28 @@ using namespace mlir;
 #include "Conversion/Passes.h.inc"
 
 namespace {
+class ToySubOpPattern : public OpConversionPattern<toy::SubOp> {
+  // using OpConversionPattern<toy::SubOp>::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(toy::SubOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    return success();
+  }
+};
+
 class ConvertToySubToMid : public ConvertToySubToMidBase<ConvertToySubToMid> {
 public:
   ConvertToySubToMid() = default;
   void runOnOperation() override {
     ConversionTarget target(getContext());
-    // target.addLegalDialect<AffineDialect, BuiltinDialect, arith::ArithDialect,
+    // target.addLegalDialect<AffineDialect, BuiltinDialect,
+    // arith::ArithDialect,
     //                        func::FuncDialect>();
     target.addIllegalOp<toy::SubOp>();
 
     RewritePatternSet patterns(&getContext());
+    patterns.add<ToySubOpPattern>(&getContext());
 
     if (failed(applyPartialConversion(getOperation(), target,
                                       std::move(patterns)))) {
