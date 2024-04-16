400c42f7e8e537c21e88352b96549dcfdc1c8501
Sabriana420 <63991830+Sabriana420@users.noreply.github.com>
2024-04-16 13:44:31 +0800

    toyToMid-Matmul (modified) (#28)
    Toy to Mid for Matmul.
    
    ---------
    
    Co-authored-by: Yuhao Jiang <jiangyuhao09@163.com>

--- include/ToyFrontend/Container.h
+++ include/ToyFrontend/Container.h
@@ -11,4 +11,12 @@ struct MemRef {
   intptr_t stride;
 };
 
+struct MemRef2d {
+  double *allocated = nullptr;
+  double *aligned = nullptr;
+  intptr_t offset = 0;
+  intptr_t size[2];
+  intptr_t stride[2];
+};
+
 #endif // TOY_COMPILER_CONTAINER_H
